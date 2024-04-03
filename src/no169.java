6a5754eebda49e143d493edb6e83056b7a23362d
Yosef1997 <yosefsitumorang97@gmail.com>
2024-04-03 16:05:11 +0700

    improve complexity no 169

--- src/no169.java
+++ src/no169.java
@@ -1,24 +1,30 @@
+import java.util.*;
+
 public class no169 {
   public static int majorityElement(int[] nums) {
-    int n = nums.length;
-    int count = 1;
+    HashMap<Integer, Integer> map = new HashMap<>();
+    float n = (float) nums.length / 2;
     int result = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        if (nums[i] == nums[j]) {
-          count++;
-        }
+    for (int i : nums) {
+      if (map.containsKey(i)) {
+        map.put(i, map.get(i) + 1);
+      } else {
+        map.put(i, 1);
       }
-      if (count > n / 2) {
-        result = nums[i];
+    }
+    System.out.println(n);
+    for (Map.Entry<Integer, Integer> set : map.entrySet()) {
+      if (set.getValue() > (n)) {
+        result = set.getKey();
+        break;
       }
     }
     return result;
   }
 
   public static void main(String[] args) {
-    int[] nums = {3,2,3};
-    int[] numss = {2,2,1,1,1,2,2};
-    System.out.println(majorityElement(numss));
+    int[] nums = {3, 2, 3};
+    int[] numss = {2, 2, 1, 1, 1, 2, 2};
+    System.out.println(majorityElement(nums));
   }
 }
